c3314ee1a6b493811a223cce245f059f24c06e95
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-25 23:33:05 -0300

    Add files via upload

--- horas_trabalho_mundo.R
+++ horas_trabalho_mundo.R
@@ -15,4 +15,36 @@
 ### por mais tempo, mas nos pultimos 150 anos o tempo de horas trabalhadas
 ### reduziu substancialmente. Particularmente, em países mais ricos.
 
+# Pacotes necessários para as análises -----------------------------------------------------------------------------------------------------
 
+library(tidyverse)
+
+# Carregar dados ---------------------------------------------------------------------------------------------------------------------------
+
+ht <- read.csv("annual-working-hours-per-worker.csv")
+view(ht)
+names(ht)
+
+# Manipulação de dados ---------------------------------------------------------------------------------------------------------------------
+
+ht1 <- ht %>%
+  select(-Code) %>%
+  rename(horas = Average.annual.working.hours.per.worker) %>%
+  filter(Entity %in% c("Angola", "Brazil", "China", "Russsia",
+                       "United States", "France", "Germany",
+                       "Ghana", "India", "Haiti", "Hungary", 
+                       "Iceland", "Israel", "Portugal", "Italy")) %>%
+  group_by(Entity) %>%
+  summarise(media = mean(horas),
+            sd = sd(horas), n = n(),
+            se = sd/sqrt(n)) %>%
+  view()
+
+ht2 <- ht %>%
+  select(-Code) %>%
+  rename(horas = Average.annual.working.hours.per.worker) %>%
+  filter(Entity %in% c("Angola", "Brazil", "China", "Russsia",
+                       "United States", "France", "Germany",
+                       "Ghana", "India", "Haiti", "Hungary", 
+                       "Iceland", "Israel", "Portugal", "Italy")) %>%
+  view()

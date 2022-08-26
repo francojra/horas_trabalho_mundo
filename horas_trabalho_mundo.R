272354ddc7605f46f205de66c63a3e43bfbdf699
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-25 23:23:01 -0300

    Add files via upload

---
+++ horas_trabalho_mundo.R
@@ -0,0 +1,18 @@
+
+# Horas de trabalho no mundo ---------------------------------------------------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data: 25/08/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/air-pollution -------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### No mundo de agitação e confusão que vivemos, é fácil assumir que estamos
+### trabalhando mais que nunca. Mas isso é verdade?
+
+### Como poderemos ver nos dados em breve, isso não é o que está acontecendo.
+
+### Os dados apresentam que no século 19 pessoas de todo o mundo trabalhavam
+### por mais tempo, mas nos pultimos 150 anos o tempo de horas trabalhadas
+### reduziu substancialmente. Particularmente, em países mais ricos.
+
+
